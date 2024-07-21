89513745dcb7f4eb4d3a436fe30ed8bf8ca6a43f
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-07-20 21:25:00 -0700

    Update debug_split.sh

--- debug_split.sh
+++ debug_split.sh
@@ -10,3 +10,13 @@ CUDA_VISIBLE_DEVICES=0 python main_split.py \
   --batch_size 8 \
   --rename_column \
   --apply_squim_quality_estimation 
+
+
+CACHE_DIR="/data/lmorove1/hwang258/sc/Speech-Captioning-Dataset/cache/out/gigaspeech-tiny-0-train-stage1"
+OUTPUT_DIR="/data/lmorove1/hwang258/sc/Speech-Captioning-Dataset/cache/out/gigaspeech-tiny-0-train-stage2"
+CSV_DIR="/data/lmorove1/hwang258/dataspeech/cache" 
+
+python annotate_split.py \
+  --cache_dir ${CACHE_DIR} \
+  --output_dir ${OUTPUT_DIR} \
+  --csv_dir ${CSV_DIR}
