ec39fc901f55209a8e61ef542ab33883d74e3547
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-11-18 00:40:05 -0500

    Update run_prompt_creation_split.py

--- scripts/run_prompt_creation_split.py
+++ scripts/run_prompt_creation_split.py
@@ -417,7 +417,7 @@ def main():
     raw_datasets_features = set(raw_datasets.features)
 
     # TODO(SG): add accent
-    EXPECTED_COLUMNS = {"pitch", "noise", "reverberation", "speech_monotony", "speaking_rate", 'age','accent','brightness','emotion','gender','smoothness'}
+    EXPECTED_COLUMNS = {"pitch", "noise", "reverberation", "speech_monotony", "speaking_rate", 'age','brightness','emotion','gender','smoothness'}
     
     if not EXPECTED_COLUMNS.issubset(raw_datasets_features):
         missing_columns = EXPECTED_COLUMNS - raw_datasets_features
